994eac1d3d7a8fe04e513929ab4205cc02dea12a
Ubuntu <ubuntu@ip-172-31-27-228.ap-southeast-1.compute.internal>
2023-02-19 06:14:00 +0000

    get term

--- src/components/semester_management/SemesterManagement.tsx
+++ src/components/semester_management/SemesterManagement.tsx
@@ -1,48 +1,116 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Table, Button } from "antd";
 import { PlusOutlined } from "@ant-design/icons";
 import classNames from "classnames/bind";
 import style from "./SemesterManagement.module.scss";
+import termService from "~/services/term";
+import moment from "moment";
 
 const cls = classNames.bind(style);
 
-const data = [
-    {
-        key: 1,
-        name: "Học kì 1",
-        time: new Date(),
-    },
-];
+interface Term{
+    id: number,
+    createdAt: Date,
+    dateDiscussion: Date,
+    dateReport: Date,
+    endDate: Date,
+    endDateChooseTopic: Date,
+    endDateSubmitTopic: Date,
+    name: Date,
+    startDate: Date,
+    startDateChooseTopic: Date,
+    startDateSubmitTopic: Date,
+    updatedAt: Date
+}
 
 const SemesterManagement = () => {
     const columns = [
         {
-            title: "Tên học kỳ",
+            title: "Tên",
             dataIndex: "name",
             key: "name",
         },
         {
-            title: "Thời gian",
-            dataIndex: "time",
-            key: "time",
-            render: (t: Date) => <div>{t.toISOString()}</div>,
+            title: "Bắt đầu",
+            dataIndex: "startDate",
+            key: "startDate",
+            render: (t: Date) => moment(t).format("DD/MM/YYYY"),
         },
-    ];
+        {
+            title: "Kết thúc",
+            dataIndex: "endDate",
+            key: "endDate",
+            render: (t: Date) => moment(t).format("DD/MM/YYYY"),
 
+        },
+        {
+            title: "Bắt đầu submit đề tài",
+            dataIndex: "startDateSubmitTopic",
+            key: "startDateSubmitTopic",
+            render: (t: Date) => moment(t).format("DD/MM/YYYY"),
+
+        },
+        {
+            title: "Kết thúc submit đề tài",
+            dataIndex: "endDateSubmitTopic",
+            key: "enendDateSubmitTopicdDate",
+            render: (t: Date) => moment(t).format("DD/MM/YYYY"),
+
+        },
+        {
+            title: "Bắt đầu chọn đề tài",
+            dataIndex: "startDateChooseTopic",
+            key: "startDateChooseTopic",
+            render: (t: Date) => moment(t).format("DD/MM/YYYY"),
+
+        },
+        {
+            title: "Kết thúc chọn đề tài",
+            dataIndex: "endDateChooseTopic",
+            key: "endDateChooseTopic",
+            render: (t: Date) => moment(t).format("DD/MM/YYYY"),
+
+        },
+        {
+            title: "Ngày phản biện",
+            dataIndex: "dateDiscussion",
+            key: "dateDiscussion",
+            render: (t: Date) => moment(t).format("DD/MM/YYYY"),
+        },
+        {
+            title: "Ngày báo cáo",
+            dataIndex: "dateReport",
+            key: "dateReport",
+            render: (t: Date) => moment(t).format("DD/MM/YYYY"),
+        }
+    ];
+    const [terms, setTerms] = useState<Array<Term>>([])
+
+    useEffect(() => {
+        termService.getTerm(1).then(result =>{
+            setTerms(result.data)            
+        })
+    }, [])
+    
     return (
         <div className={cls("semester_management")}>
             <div className={cls("semester_func")}>
-                <h4 className={cls('semester_title')}>Quản lý học kì</h4>
+                <h4 className={cls("semester_title")}>Quản lý học kì</h4>
                 <Button
                     type="dashed"
                     icon={<PlusOutlined />}
                     size="large"
-                    style={{ marginBottom: "10px", animation: "none", color:'rgb(80, 72, 229)', fontWeight: "600" }}
+                    style={{
+                        marginBottom: "10px",
+                        animation: "none",
+                        color: "rgb(80, 72, 229)",
+                        fontWeight: "600",
+                    }}
                 >
                     Tạo
                 </Button>
             </div>
-            <Table columns={columns} dataSource={data} />
+            <Table columns={columns} dataSource={terms} />
         </div>
     );
 };
