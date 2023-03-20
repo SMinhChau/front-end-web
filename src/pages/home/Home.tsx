f09ffa354ac515459510d7c22b23d059a075ac1a
SMinhChau <ubuntu@no-reply.vndev.info>
2023-03-20 13:08:29 +0000

    update image slide home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -10,72 +10,55 @@ import Slider from "react-slick";
 import slide1 from "~/assets/home/sl01.jpg";
 import slide2 from "~/assets/home/sl02.png";
 import "./Home.css";
+import contentHome from "~/pages/home/content";
 
 const cls = classNames.bind(style);
 
 const Home = () => {
-    const userState = useAppSelector((state) => state.user);
-    const navigate = useNavigate();
-    const settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-    };
-    useEffect(() => {
-        if (userState.is_login && userState.functions.length) {
-            navigate(userState.functions[0].url);
-        }
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [userState]);
+  const userState = useAppSelector((state) => state.user);
+  const navigate = useNavigate();
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+  useEffect(() => {
+    if (userState.is_login && userState.functions.length) {
+      navigate(userState.functions[0].url);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [userState]);
 
-    return (
-        <div className={cls("home_page")}>
-            <AppHeader />
-            <div className={cls("home_body")}>
-                <Slider {...settings}>
-                    <div className={cls("slide_item")}>
-                        <div>
-                            <h2>
-                                Hệ Thống Đăng Ký Khóa Luận Tốt Nghiệp Online
-                            </h2>
-                            <span>
-                                Đổi mới, nâng tầm cao mới - Năng động, hội nhập
-                                toàn cầu
-                            </span>
-                        </div>
-                        <img src={slide2} alt="" />
-                    </div>
-                    <div className={cls("slide_item")}>
-                        <div>
-                            <h2>
-                                Hệ Thống Đăng Ký Khóa Luận Tốt Nghiệp Online
-                            </h2>
-                            <span>
-                                Đổi mới, nâng tầm cao mới - Năng động, hội nhập
-                                toàn cầu
-                            </span>
-                        </div>
-                        <img src={slide1} alt="" />
-                    </div>
-                </Slider>
-                <div className={cls("content")}>
-                    <h1>Giới thiệu</h1>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Molestiae dolore, perspiciatis quia architecto officia
-                        veritatis ea totam eos sunt vitae iste doloribus sint
-                        sapiente perferendis? Nulla quo sint aliquam rerum!
-                        Lorem ipsum dolor sit amet, consectetur adipisicing
-                        elit. Sint odit quo et inventore, temporibus ad deleniti
-                        fuga accusantium corrupti corporis eaque ea maiores aut
-                        nobis, tempore dignissimos nostrum laboriosam neque?
-                    </p>
-                </div>
+  return (
+    <div className={cls("home_page")}>
+      <AppHeader />
+      <div className={cls("home_body")}>
+        <Slider {...settings}>
+          <div className={cls("slide_item")}>
+            <div>
+              <h2>{contentHome.title}</h2>
+              <span>{contentHome.slide}</span>
             </div>
+            <img src={slide2} alt="" />
+          </div>
+          <div className={cls("slide_item")}>
+            <div>
+              <h2>{contentHome.title}</h2>
+              <span>{contentHome.slide}</span>
+            </div>
+            <img src={slide1} alt="" />
+          </div>
+        </Slider>
+        <div className={cls("content")}>
+          <h1>Giới thiệu</h1>
+          <h2 className={cls("content_title")}>{contentHome.title}</h2>
+          <p>{contentHome.main}</p>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Home;
